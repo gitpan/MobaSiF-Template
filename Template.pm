d068670a174d865bb9d40b951617f702cd030ab1
Tadashi Matsuda <bobpp@bobpp.jp>
2009-09-09 02:56:46 +0000

    Import of BOBPP/MobaSiF-Template-0.01 from CPAN.
    gitpan-cpan-distribution: MobaSiF-Template
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         BOBPP/MobaSiF-Template-0.01.tar.gz
    gitpan-cpan-author:       BOBPP
    gitpan-cpan-maturity:     released

---
+++ Template.pm
@@ -0,0 +1,59 @@
+package MobaSiF::Template;
+
+use 5.008;
+use strict;
+use warnings;
+
+use File::stat;
+use MobaSiF::Template::Compiler;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+our %EXPORT_TAGS = ( 'all' => [ qw() ] );
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+our @EXPORT = qw();
+our $VERSION = '0.01';
+
+require XSLoader;
+XSLoader::load('MobaSiF::Template', $VERSION);
+
+our $DEVELOP = 0;
+sub render {
+	my ($template, $compiled_template, $param, $param2, $param3) = @_;
+	if ($DEVELOP) {
+		my $template_stat = stat($template);
+		my $compiled_stat = stat($compiled_template);
+
+		if ($template_stat->mtime > $compiled_stat->mtime) {
+			MobaSiF::Template::Compiler::compile($template, $compiled_template);
+		}
+	}
+	return MobaSiF::Template::insert($compiled_template, $param, $param2, $param3);
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+MobaSiF::Template - Very fast Template module written by XS.
+
+=head1 SYNOPSIS
+
+use MobaSiF::Template;
+$html = MobaSiF::Template::insert($compiled_template_file, $refParamHash);
+
+=head1 ABSTRACT
+
+事前コンパイルされたテンプレートバイナリを用いて高速なテンプレート処理を行います。
+ループ・条件分岐・置換（URL ENCODE, HTMLSPECIALCHARS(+NL2BR) が可能）に対応しており、基本的なHTMLテンプレートの処理に対応できます。
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+MobaSiF::Template::Compiler
+
+=cut
